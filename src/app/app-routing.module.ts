26297c6a727ac6ed97e5783ef8d0055bfead5adf
Patocskai Roland <h671741@stud.u-szeged.hu>
2020-03-24 10:44:18 +0100

    routing debug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { ProfilePageModule } from './main/profile/profile.module';
 import { StaffPageModule } from './main/staff/staff.module';
 import { GroupStatisticsPageModule } from './main/group-statistics/group-statistics.module';
 import { ExaminationPageModule } from './main/examination/examination.module';
-import { ShareModule } from './shared/share.module';
 import { AuthGuard } from './core/auth.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -11,24 +10,31 @@ import { WallPageModule } from './main/wall/wall.module';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
-    path: 'wall', loadChildren: './wall/wall.module#WallPageModule', canActivate: [AuthGuard]
+    path: 'wall',
+     loadChildren: './main/wall/wall.module#WallPageModule', canActivate: [AuthGuard]
   },
   {
-    path: 'examination', loadChildren: './examination/examination.module#ExaminationPageModule', canActivate: [AuthGuard]
+    path: 'examination',
+     loadChildren: './main/examination/examination.module#ExaminationPageModule', canActivate: [AuthGuard]
   },
   {
-    path: 'group-statistics', loadChildren: './group-statistics/group-statistics.module#GroupStatisticsPageModule',
+    path: 'group-statistics',
+    loadChildren: './main/group-statistics/group-statistics.module#GroupStatisticsPageModule', canActivate: [AuthGuard]
   },
   {
-    path: 'staff', loadChildren: './staff/staff.module#StaffPageModule', canActivate: [AuthGuard]
+    path: 'staff',
+     loadChildren: './main/staff/staff.module#StaffPageModule', canActivate: [AuthGuard]
   },
   {
-    path: 'histological', loadChildren: './examination/examination.module#ExaminationPageModule', canActivate: [AuthGuard]
+    path: 'histological',
+     loadChildren: './main/examination/examination.module#ExaminationPageModule', canActivate: [AuthGuard]
   },
   {
-    path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule', canActivate: [AuthGuard]
+    path: 'profile',
+     loadChildren: './main/profile/profile.module#ProfilePageModule', canActivate: [AuthGuard]
   },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'login',
+   loadChildren: './login/login.module#LoginPageModule' },
   { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];
 
