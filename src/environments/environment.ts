cde85f7c96f325e8c41047983e1c4b009d995c28
Patocskai Roland <h671741@stud.u-szeged.hu>
2020-04-08 15:40:08 +0200

    image replace, new firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,14 @@ const settings = { timestampsInSnapshots: true };
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCAky8hhcBtC8H7kbBjCS7ECFPgxj1MBYU',
-    authDomain: 'dermiq-management.firebaseapp.com',
-    databaseURL: 'https://dermiq-management.firebaseio.com',
-    projectId: 'dermiq-management',
-    storageBucket: 'dermiq-management.appspot.com',
-    messagingSenderId: '226471477014'
+    apiKey: 'AIzaSyDLbcapoIS7C6de7uSuvhfubVa8n_hxYn8',
+    authDomain: 'fog-of-d.firebaseapp.com',
+    databaseURL: 'https://fog-of-d.firebaseio.com',
+    projectId: 'fog-of-d',
+    storageBucket: 'fog-of-d.appspot.com',
+    messagingSenderId: '271441681421',
+    appId: '1:271441681421:web:c66c918f05b759e15a8944',
+    measurementId: 'G-ZMC2CYMSYF'
   }
 };
+
