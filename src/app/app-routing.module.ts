602293f9ff62ca5b9b95aba1ce8b3201807b6080
Patocskai Roland <h671741@stud.u-szeged.hu>
2020-04-09 14:56:26 +0200

    histological debug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ExaminationPage } from './main/examination/examination.page';
 import { ProfilePageModule } from './main/profile/profile.module';
 import { StaffPageModule } from './main/staff/staff.module';
 import { GroupStatisticsPageModule } from './main/group-statistics/group-statistics.module';
@@ -11,31 +12,37 @@ const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'wall',
-     loadChildren: './main/wall/wall.module#WallPageModule', canActivate: [AuthGuard]
+    loadChildren: './main/wall/wall.module#WallPageModule',
+    canActivate: [AuthGuard],
   },
   {
     path: 'examination',
-     loadChildren: './main/examination/examination.module#ExaminationPageModule', canActivate: [AuthGuard]
+    loadChildren: './main/examination/examination.module#ExaminationPageModule',
+    canActivate: [AuthGuard],
   },
   {
     path: 'group-statistics',
-    loadChildren: './main/group-statistics/group-statistics.module#GroupStatisticsPageModule', canActivate: [AuthGuard]
+    loadChildren:
+      './main/group-statistics/group-statistics.module#GroupStatisticsPageModule',
+    canActivate: [AuthGuard],
   },
   {
     path: 'staff',
-     loadChildren: './main/staff/staff.module#StaffPageModule', canActivate: [AuthGuard]
-  },
-  {
-    path: 'histological',
-     loadChildren: './main/examination/examination.module#ExaminationPageModule', canActivate: [AuthGuard]
+    loadChildren: './main/staff/staff.module#StaffPageModule',
+    canActivate: [AuthGuard],
   },
   {
     path: 'profile',
-     loadChildren: './main/profile/profile.module#ProfilePageModule', canActivate: [AuthGuard]
+    loadChildren: './main/profile/profile.module#ProfilePageModule',
+    canActivate: [AuthGuard],
   },
-  { path: 'login',
-   loadChildren: './login/login.module#LoginPageModule' },
-  { path: '**', redirectTo: 'login', pathMatch: 'full' }
+  {
+    path: 'histological',
+    component: ExaminationPage,
+    canActivate: [AuthGuard],
+  },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' },
 ];
 
 @NgModule({
@@ -45,8 +52,8 @@ const routes: Routes = [
     ExaminationPageModule,
     GroupStatisticsPageModule,
     StaffPageModule,
-    ProfilePageModule
+    ProfilePageModule,
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/services/diagnosis.service.ts
+++ src/app/services/diagnosis.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 
 export interface Diagnosis {
+  category: string;
+  code: string;
   text: string;
-  ordinal: number;
 }
 
 @Injectable({
@@ -23,19 +24,14 @@ export class DiagnosisService {
   getAllDiagnosis() {
     this.diagnosisCollection.valueChanges().subscribe((data) => {
       this.diagList = [];
-      data.sort((a, b) => {
-        if (a.ordinal == b.ordinal) return 0;
-        if (a.ordinal > b.ordinal) return 1;
-        if (b.ordinal > a.ordinal) return -1;
-      });
       data.forEach((diag) => {
         this.diagList.push(diag.text);
       });
     });
   }
 
-  addDiagnosis(diag: string, ordinal: number): Promise<firebase.firestore.DocumentReference> {
-    return this.diagnosisCollection.add({ text: diag, ordinal: ordinal });
+  addDiagnosis(diag: string, category: string, code: string): Promise<firebase.firestore.DocumentReference> {
+    return this.diagnosisCollection.add({ category, code, text: diag });
   }
 
 }
